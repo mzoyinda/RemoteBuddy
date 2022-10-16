f6f4e9fe65414007fd0c8c31818a0a9bfe18f7ba
Gbemisola Kotoye <barkty@yahoo.com>
2022-10-16 05:01:01 +0100

    feat: Add select libraries

---
+++ src/pages/choose/Choose.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { ChooseContainer } from './styles'
+import mack from 'assets/audiomack.png'
+import boom from 'assets/boomplay.png'
+import spotify from 'assets/spotify.png'
+import youtube from 'assets/youtube.png'
+
+const Choose = () => {
+
+  return (
+    <ChooseContainer>
+        <h3 className='text'>Jane, would you like to</h3>
+        <button type='button'>
+            Use our music Library
+        </button>
+        <div className='line_contain'>
+            <div className='line'></div>
+            <p className='line_text'>Or choose from your favorite Muisc apps below</p>
+            <div className='line'></div>
+        </div>
+
+        <div className='libraries_wrap'>
+            <div className='library'>
+                <img alt='AudioMack' src={mack}/>
+            </div>
+            <div className='library'>
+                <img alt='Boomplay' src={boom}/>
+            </div>
+            <div className='library'>
+                <img alt='Spotify' src={spotify}/>
+            </div>
+            <div className='library'>
+                <img alt='Youtube' src={youtube}/>
+            </div>
+        </div>
+    </ChooseContainer>
+  )
+}
+
+export default Choose
