8ae7dc7f0f924674481ea349638d600e0de21e1d
oyindamola <oyindamoladawodu22@gmail.com>
2022-10-16 04:35:54 +0100

    animation added to login page * framer motion

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import Navbar from 'components/navbar/Navbar';
-import { Layout } from 'layout/Layout';
-import React from 'react';
-import './App.css';
+import Navbar from "components/navbar/Navbar";
+import { Layout } from "layout/Layout";
+import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <Layout>
-        
+
       </Layout>
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,15 +4,27 @@ import Logo from "../assets/maxi-logo.png";
 import Spark from "../assets/lightening.png";
 import Sun from "../assets/sun.png";
 import Remote from '../assets/onboard.png';
+import { motion} from "framer-motion"
 
 const Login = () => {
   return (
-    <Container>
+    <Container    className="box"    >
+        
       <SideBar>
         <img src={Spark} alt="spark" className="spark" />
         <img src={Logo} alt="maxi-logo" className="logo" />
       </SideBar>
       <MainField>
+        <motion.div
+             initial={{ opacity: 0, scale: 0.5 }}
+             animate={{ opacity: 1, scale: 1 }}
+             transition={{
+               duration: 0.8,
+               delay: 0.5,
+               ease: [0, 0.71, 0.2, 1.01]
+             }}
+        >
+
         <Hello>
           <p>HELLO, Remote Worker .</p>
           <img src={Sun} alt="sun" className="sun" />
@@ -25,6 +37,7 @@ const Login = () => {
         <ImgContainer>
         <img src={Remote} alt="lady working on laptop" />
         </ImgContainer>
+</motion.div>
       </MainField>
     </Container>
   );
@@ -45,12 +58,12 @@ position: relative;
   justify-content: center;
 
   .logo {
-    width: 376px;
+    width: 75%;
   }
 
   .spark {
     position: absolute;
-    width: 142.5px;
+    width: 25%;
     right: 0;
     top: 0;
   }
