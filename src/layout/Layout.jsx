00435e1184ea110c66e0e9aea03f9776719240d3
oyindamola <oyindamoladawodu22@gmail.com>
2022-10-16 07:35:22 +0100

    work so far

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,6 +5,8 @@ import { Back } from 'components/button/Back'
 import background from 'assets/background.png'
 import Card from 'components/card/Card'
 
+import { motion, AnimatePresence} from "framer-motion"
+
 export const Layout = ({ children }) => {
 
   return (
@@ -12,11 +14,19 @@ export const Layout = ({ children }) => {
       <Navbar/>
       <div className='wrap'>
         <Back/>
-        <div className='card_contain'>
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className='card_contain'
+          >
           <Card>
             {children}
           </Card>
-        </div>
+        </motion.div>
+        </AnimatePresence>
       </div>  
     </LayoutContain>
   )
---
+++ src/pages/Welcome.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Container } from './styles'
+import { Layout } from 'layout/Layout'
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Welcome = () => {
+
+  return (
+    <Layout>
+        <Container>
+            <h3 className='text'>Hi, i'm Buddy, What should i call you?</h3>
+            <Input type="text" /> 
+            <button type='button'>
+            <Link to='/remind'> Got it!</Link>
+            </button> 
+        </Container>
+
+    </Layout>
+  )
+}
+
+const Input = styled.input`
+border:none;
+width:70%;
+border-bottom: 1px solid #828282;
+`
+
+export default Welcome
